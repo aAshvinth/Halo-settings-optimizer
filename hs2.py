7ddf81caff301d0aa0ea12df98bb0e4048f0cc42
Ashvinth <106897514+aAshvinth@users.noreply.github.com>
2022-08-05 06:53:58 +0000

    Update hs2.py

--- hs2.py
+++ hs2.py
@@ -1,23 +1,19 @@
-config = open(r'halo.txt', 'r')
+with open('SpecControlSettings.json', 'r', encoding='utf-8') as file:
+    settings = file.readlines()
 
-# Reads the config file
+settings[47] = '        "value": 960 \n'
+settings[51] = '        "value": 960 \n'
+settings[55] = '        "value": 0 \n'
+settings[535] = '        "value": "Low" \n '
+settings[539] = '        "value": "Low" \n'
+settings[543] = '        "value": "Low" \n'
+settings[555] = '        "value": "Low" \n'
+settings[563] = '        "value": "Low" \n'
+settings[567] = '        "value": "Low" \n'
+settings[571] = '        "value": "Low" \n'
+settings[599] = '        "value": "Low" \n'
+settings[607] = '        "value": "Low" \n'
+settings[623] = '        "value": "Low" \n'
 
-settings = config.readlines()
-
-# changes the settings for the best fps
-
-settings[47] = """        "value": 960 """
-settings[51] = """        "value": 960 """
-settings[55] = """        "value": 0 """
-settings[535] = """        "value": "Low" """
-settings[539] = """        "value": "Low" """
-settings[543] = """        "value": "Low" """
-settings[555] = """        "value": "Low" """
-settings[563] = """        "value": "Low" """
-settings[567] = """        "value": "Low" """
-settings[571] = """        "value": "Low" """
-settings[599] = """        "value": "Low" """
-settings[607] = """        "value": "Low" """
-settings[623] = """        "value": "Low" """
-with open(r'halo.txt', 'w') as file:
+with open('SpecControlSettings.json', 'w', encoding='utf-8') as file:
     file.writelines(settings)
