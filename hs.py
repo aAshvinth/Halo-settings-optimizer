0dccae699c23ce71edec6a756ac7fad8bc1db3a2
Ashvinth <106897514+aAshvinth@users.noreply.github.com>
2022-08-06 15:47:26 +0000

    Update hs.py

--- hs.py
+++ hs.py
@@ -1,8 +1,7 @@
 with open('SpecControlSettings.json', 'r', encoding='utf-8') as file:
     settings = file.readlines()
-try:
-    fps = int(input('Input your monitors refresh rate:'))
-except ValueError:
+fps = int(input('Input your monitors refresh rate:'))
+
     print('Only enter numbers (example: if you have a 60hz display enter 60)')
 settings[47] = '        "value": ' + fps + '\n'
 settings[535] = '        "value": "Low" \n '
