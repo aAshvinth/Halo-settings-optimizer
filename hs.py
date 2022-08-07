8e5a640e388c6a25e02d9113f2cabb32e86c09d1
Ashvinth <106897514+aAshvinth@users.noreply.github.com>
2022-08-07 07:56:06 +0000

    Update hs.py

--- hs.py
+++ hs.py
@@ -3,21 +3,27 @@ with open('SpecControlSettings.json', 'r', encoding='utf-8') as file:
 fps = input('Input your monitors refresh rate:')
 
 settings[47] = '        "value": ' + fps + '\n'
-settings[131] = '        "value": 0 \n '
-settings[159] = '        "value": 0 \n '
-settings[463] = '        "value": 0 \n '
-settings[467] = '        "value": 0 \n '
-settings[475] = '        "value": 0 \n '
-settings[535] = '        "value": "Low" \n'
+settings[375] = '        "value": 0 \n'
+settings[463] = '        "value": 0 \n'
+settings[467] = '        "value": 0 \n'
+settings[475] = '        "value": 0 \n'
+settings[535] = '        "value": "Low" \n '
 settings[539] = '        "value": "Low" \n'
 settings[543] = '        "value": "Low" \n'
+settings[547] = '        "value": "Low" \n'
 settings[555] = '        "value": "Low" \n'
 settings[563] = '        "value": "Low" \n'
 settings[567] = '        "value": "Low" \n'
 settings[571] = '        "value": "Low" \n'
+settings[579] = '        "value": "off" \n'
 settings[599] = '        "value": "Low" \n'
+settings[603] = '        "value": "Low" \n'
 settings[607] = '        "value": "Low" \n'
+settings[611] = '        "value": "off" \n'
+settings[615] = '        "value": "Low" \n'
+settings[619] = '        "value": "Low" \n'
 settings[623] = '        "value": "Low" \n'
+settings[635] = '        "value": "Low" \n'
 
 with open('SpecControlSettings.json', 'w', encoding='utf-8') as file:
     file.writelines(settings)
